770b1b7d33d81fdb32f2e9b604e63f78459aff77
YoungChulshin <go1323@gmail.com>
2020-08-06 16:52:22 +0900

    recentTacos에 HATEOAS 추가

--- taco-cloud/src/main/java/tacos/web/api/DesignTacoApiController.java
+++ taco-cloud/src/main/java/tacos/web/api/DesignTacoApiController.java
@@ -3,13 +3,22 @@ package tacos.web.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/design", produces = "application/json")
@@ -21,9 +30,25 @@ public class DesignTacoApiController {
 
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos() {
+    public CollectionModel<EntityModel<Taco>> recentTacos() {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
-        return tacoRepository.findAll(page).getContent();
+        List<Taco> tacos = tacoRepository.findAll(page).getContent();
+
+        CollectionModel<EntityModel<Taco>> recentResources = CollectionModel.wrap(tacos);
+
+        // Step1
+        //recentResources.add(new Link("http://localhost:8080/design/recent", "recents"));
+
+        // Step2
+//        recentResources.add(
+//                WebMvcLinkBuilder.linkTo(DesignTacoApiController.class).slash("recent").withRel("recent")); // 링크의 관계 이름
+
+        // Step3
+        recentResources.add(
+                linkTo(methodOn(DesignTacoApiController.class).recentTacos())
+                .withRel("recents"));
+
+        return recentResources;
     }
 
     @GetMapping("/{id}")
